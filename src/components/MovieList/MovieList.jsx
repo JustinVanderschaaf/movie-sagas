2e9ba78ecde22b000827aab9d625d92a407ccdb2
Justin Vanderschaaf <Vanderschaaf.usmc@gmail.com>
2022-01-29 11:05:39 -0600

    added on click to the movies img tag to send the clicked movie data to function

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,32 +1,40 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import './MovieList.css'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./MovieList.css";
 
 function MovieList() {
+  const dispatch = useDispatch();
+  const movies = useSelector((store) => store.movies);
 
-    const dispatch = useDispatch();
-    const movies = useSelector(store => store.movies);
+  useEffect(() => {
+    dispatch({ type: "FETCH_MOVIES" });
+  }, []);
 
-    useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES' });
-    }, []);
+  function getDetails(movie) {
+    console.log("click", movie.id);
+  }
 
-    return (
-        <main>
-            <h1>MovieList</h1>
-            <section className="movies">
-                {movies.map(movie => {
-                    return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
-                        </div>
-                    );
-                })}
-            </section>
-        </main>
-
-    );
+  return (
+    <main>
+      <h1>MovieList</h1>
+      <section className="movies">
+        {movies.map((movie) => {
+          return (
+            //
+            <div key={movie.id}>
+              <h3>{movie.title}</h3>
+              {/* on click sends the id of the movie clicked to function listed */}
+              <img
+                src={movie.poster}
+                alt={movie.title}
+                onClick={() => getDetails(movie)}
+              />
+            </div>
+          );
+        })}
+      </section>
+    </main>
+  );
 }
 
 export default MovieList;
